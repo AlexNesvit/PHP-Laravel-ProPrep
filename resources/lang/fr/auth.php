3b293347e3c335863853b0768d8b6bb6eb1efc52
AlexNesvit <alex.nesvit2000@gmail.com>
2024-10-29 10:58:38 +0100

    lang fr

---
+++ resources/lang/fr/auth.php
@@ -0,0 +1,7 @@
+<?php
+// resources/lang/fr/auth.php
+return [
+    'failed' => 'Les informations d\'identification ne sont pas correctes.',
+    'password' => 'Le mot de passe est incorrect.',
+    'throttle' => 'Trop de tentatives. Veuillez réessayer dans :seconds secondes.',
+];
