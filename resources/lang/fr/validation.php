b16404b1972f152f7658b472a7acad40df80d676
AlexNesvit <alex.nesvit2000@gmail.com>
2024-11-01 13:07:56 +0100

    HomeController+lang fr

---
+++ resources/lang/fr/validation.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'accepted'             => 'Le champ :attribute doit être accepté.',
+    'active_url'           => "Le champ :attribute n'est pas une URL valide.",
+    'after'                => 'Le champ :attribute doit être une date postérieure au :date.',
+    // ... другие строки валидации ...
+    'attributes'           => [],
+];
