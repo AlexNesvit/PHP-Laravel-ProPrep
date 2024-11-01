797d7d4dd0260a8114ea5ec4db573feffe2fbaf0
AlexNesvit <alex.nesvit2000@gmail.com>
2024-11-01 13:57:34 +0100

    views/layouts/welcome.blade.php

--- resources/lang/fr/messages.php
+++ resources/lang/fr/messages.php
@@ -4,5 +4,6 @@ return [
     'home_title'       => 'Page d\'accueil',
     'welcome_message'  => 'Bienvenue sur la plateforme de préparation aux entretiens PHP',
     'intro_text'       => 'Cette plateforme vous aidera à vous préparer efficacement aux entretiens techniques en PHP.',
+    'app_name' => 'Plateforme de Préparation aux Entretiens PHP',
     // Добавьте другие строки по мере необходимости
 ];
