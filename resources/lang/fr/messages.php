34d0decba200024d35c14ef16586165d7d79320a
AlexNesvit <alex.nesvit2000@gmail.com>
2024-11-01 13:29:50 +0100

    lang/fr/messages

---
+++ resources/lang/fr/messages.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'home_title'       => 'Page d\'accueil',
+    'welcome_message'  => 'Bienvenue sur la plateforme de préparation aux entretiens PHP',
+    'intro_text'       => 'Cette plateforme vous aidera à vous préparer efficacement aux entretiens techniques en PHP.',
+    // Добавьте другие строки по мере необходимости
+];
