97ec7032c401a58341e218e2e53d1598aaaedc8e
AlexNesvit <alex.nesvit2000@gmail.com>
2024-11-07 20:10:10 +0100

    corrV4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ __('welcome_ok_page') }}</title>
+        <title>{{ __('app_name') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
